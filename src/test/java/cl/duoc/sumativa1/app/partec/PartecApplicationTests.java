c0f0d4f0602a4ceb4a344e1b5950d5a90c83fc56
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-23 15:16:52 -0300

    primer commit con estructura básica

---
+++ src/test/java/cl/duoc/sumativa1/app/partec/PartecApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.sumativa1.app.partec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
