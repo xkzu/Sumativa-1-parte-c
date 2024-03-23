0eb3a9ac003e2496672ef47458d6af8eeed9db0e
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-23 20:54:59 -0300

    se agrega metodo para obtener el promedio

--- src/main/java/cl/duoc/sumativa1/app/partec/service/Publication.java
+++ src/main/java/cl/duoc/sumativa1/app/partec/service/Publication.java
@@ -3,10 +3,13 @@ package cl.duoc.sumativa1.app.partec.service;
 import cl.duoc.sumativa1.app.partec.model.Publishing;
 
 import java.util.List;
+import java.util.Map;
 
 public interface Publication {
 
     List<Publishing> getPublications();
 
     Publishing getPublication(int id);
+
+    Map<String, String> getAverage(int id);
 }
