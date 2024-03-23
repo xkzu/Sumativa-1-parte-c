7c714a3cd2bdadf6659647849f3f6b7b471953dd
Kzu <alvaro.vega.pavez@gmail.com>
2024-03-23 16:37:55 -0300

    se crea clase User

---
+++ src/main/java/cl/duoc/sumativa1/app/partec/model/User.java
@@ -0,0 +1,14 @@
+package cl.duoc.sumativa1.app.partec.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class User {
+
+    private int id;
+
+    private String name;
+
+}
