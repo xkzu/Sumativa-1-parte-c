c11df3a06d45e6fec686bb71df8b03dcb6e73a4f
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 17:42:33 -0400

    se crea endpoint addComment

--- src/main/java/cl/duoc/sumativa1/app/partec/controller/CommentController.java
+++ src/main/java/cl/duoc/sumativa1/app/partec/controller/CommentController.java
@@ -1,12 +1,18 @@
 package cl.duoc.sumativa1.app.partec.controller;
 
+import cl.duoc.sumativa1.app.partec.model.Comment;
+import cl.duoc.sumativa1.app.partec.model.CommentResponse;
 import cl.duoc.sumativa1.app.partec.model.CommentsResponse;
 import cl.duoc.sumativa1.app.partec.service.CommentService;
+import cl.duoc.sumativa1.app.partec.service.PublicationService;
 import cl.duoc.sumativa1.app.partec.util.Constant;
+import cl.duoc.sumativa1.app.partec.util.ValidateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +20,12 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    private final PublicationService publicationService;
+
     @Autowired
-    public CommentController(CommentService commentService) {
+    public CommentController(CommentService commentService, PublicationService publicationService) {
         this.commentService = commentService;
+        this.publicationService = publicationService;
     }
 
     @GetMapping("/comments/{id}")
@@ -34,6 +43,26 @@ public class CommentController {
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body(
                     new CommentsResponse("Error al buscar comentarios " + e.getMessage(), null));
+        }
+    }
+
+    @PostMapping("comments/add")
+    public ResponseEntity<CommentResponse> addComment(@RequestBody Comment comment) {
+        try {
+            if (comment.getIdPublication() < 1 || ValidateUtil.isEmptyOrNull(comment.getComments())) {
+                return ResponseEntity.badRequest().body(
+                        new CommentResponse(
+                                "idPublicacion debe ser mayor a cero y comentarios no pueden ser vacio ni null",
+                                null));
+            }
+            if (publicationService.getPublication(comment.getIdPublication()).isEmpty()) {
+                return ResponseEntity.ofNullable(
+                        new CommentResponse("idPublication no existe en bd", null));
+            }
+            return ResponseEntity.ok(new CommentResponse(Constant.SUCCESS, commentService.addComment(comment)));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(
+                    new CommentResponse("Error al agregar comentario " + e.getMessage(), null));
         }
     }
 }
