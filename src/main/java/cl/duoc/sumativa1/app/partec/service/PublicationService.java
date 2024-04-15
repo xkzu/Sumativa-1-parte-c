52b6528185dc119410a3a22f86238cdd30218bc8
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 04:16:26 -0400

    se crea metodo addPublication

--- src/main/java/cl/duoc/sumativa1/app/partec/service/PublicationService.java
+++ src/main/java/cl/duoc/sumativa1/app/partec/service/PublicationService.java
@@ -3,7 +3,6 @@ package cl.duoc.sumativa1.app.partec.service;
 import cl.duoc.sumativa1.app.partec.model.Publication;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 public interface PublicationService {
@@ -12,5 +11,5 @@ public interface PublicationService {
 
     Optional<Publication> getPublication(Long id);
 
-    Map<String, String> getAverage(Long id);
+    Publication addPublication(Publication publication);
 }
