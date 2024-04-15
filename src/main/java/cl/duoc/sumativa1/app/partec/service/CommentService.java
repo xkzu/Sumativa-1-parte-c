e7b636aacb9b4fb1532d2cfd455bb6385f48f962
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 17:21:45 -0400

    se crea interface CommentService

---
+++ src/main/java/cl/duoc/sumativa1/app/partec/service/CommentService.java
@@ -0,0 +1,10 @@
+package cl.duoc.sumativa1.app.partec.service;
+
+import cl.duoc.sumativa1.app.partec.model.Comment;
+
+import java.util.List;
+
+public interface CommentService {
+
+    List<Comment> getCommnetsById(Long id);
+}
