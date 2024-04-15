de6ac47784eb5e4aef48d127a650a0a35dc5d690
Kzu <alvaro.vega.pavez@gmail.com>
2024-04-15 08:47:27 -0400

    se crean metodos en interface

--- src/main/java/cl/duoc/sumativa1/app/partec/service/QualificationService.java
+++ src/main/java/cl/duoc/sumativa1/app/partec/service/QualificationService.java
@@ -4,6 +4,7 @@ import cl.duoc.sumativa1.app.partec.model.Qualification;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public interface QualificationService {
 
@@ -12,4 +13,10 @@ public interface QualificationService {
     Qualification add(Qualification qualification);
 
     List<Qualification> getQualificationsByIdPublication(Long id);
+
+    List<Qualification> getQualifications();
+
+    Optional<Qualification> getQulification(Long id);
+
+    void deleteQualification(Long id);
 }
